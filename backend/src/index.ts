1ccf3704c72b5a03cb108be547ae5671e2fbd15f
Harshwardhan Deshmukh <harshwardhanet7007@gmail.com>
2025-05-02 00:17:45 +0530

    added few routes - users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
-import { getPrisma } from "./utils";
-import { decode, sign, verify } from "hono/jwt";
+import { getPrisma, hashPassword, verifyPassword, } from "./utils";
+import { sign, verify } from "hono/jwt";
 import { cors } from "hono/cors";
 
 type Bindings = {
@@ -8,15 +8,17 @@ type Bindings = {
   JWT_SECRET: string;
 };
 
+type Variables = {
+  userId: string
+}
+
 interface SignUpBody {
   email: string;
   name?: string;
   password: string;
 }
 
-const app = new Hono<{
-  Bindings: Bindings;
-}>();
+const app = new Hono<{ Bindings: Bindings, Variables: Variables }>();
 
 app.use("/api/v1/*", cors());
 
@@ -29,28 +31,36 @@ app.post("/api/v1/signup", async (c) => {
   // you need to initialize prisma again and again in every route (connection to database)
   // you dont have access to environment variable outside routes (in serverless)
   const prisma = getPrisma(c.env.DATABASE_URL);
+  const hash = await hashPassword(body.password);
 
-  const response = await prisma.user.create({
-    data: {
-      email: body.email,
-      password: body.password,
-      name: body.name ? body.name : undefined,
-    },
-    select: {
-      id: true,
-      email: true,
-      name: true,
-    },
-  });
+  try {
+    const response = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hash,
+        name: body.name ? body.name : undefined,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+      },
+    });
+    const token = await sign(response, c.env.JWT_SECRET);
+    let success = false;
+    if (token) success = true;
 
-  const token = await sign(response, c.env.JWT_SECRET);
-  let success = false;
-  if (token) success = true;
-
-  return c.json({
-    success,
-    jwt: token,
-  });
+    return c.json({
+      success,
+      jwt: token,
+    });
+  } catch (e) {
+    c.status(411);
+    return c.json({
+      success: false,
+      message: "Username already exists"
+    })
+  }
 });
 
 type SignInBody = Pick<SignUpBody, "email" | "password">;
@@ -59,16 +69,11 @@ app.post("/api/v1/signin", async (c) => {
   const body: SignInBody = await c.req.json();
   const prisma = getPrisma(c.env.DATABASE_URL);
 
+  // check if user exists
   const response = await prisma.user.findUnique({
     where: {
       email: body.email,
-      password: body.password,
-    },
-    select: {
-      id: true,
-      email: true,
-      name: true,
-    },
+    }
   });
 
   if (!response) {
@@ -77,27 +82,49 @@ app.post("/api/v1/signin", async (c) => {
       success: false,
       message: "User not found",
     });
+  } else {
+
+    const isValidPassword = await verifyPassword(response.password, body.password);
+    
+    if (isValidPassword) {
+      const token = await sign({
+        id: response.id,
+        email: response.email,
+        name: response.name,
+      }, c.env.JWT_SECRET);
+
+      return c.json({
+        success: true,
+        jwt: token,
+      });
+    } else {
+      return c.json({
+        success: false,
+        message: "Invalid password"
+      });
+    }
   }
-
-  const token = await sign(response, c.env.JWT_SECRET);
-  let success = false;
-  if (token) success = true;
-
-  return c.json({
-    success,
-    jwt: token,
-  });
 });
 
-app.use("/api/v1/blog/*", async (c, next) => {
-  const header = c.req.header("Authorization") || "";
-  const token = header.split(" ")[1];
-  const response = await verify(token, c.env.JWT_SECRET);
-  if (!response.id) {
-    c.status(403);
-    return c.json({ error: "unauthorized" });
+// Authentication Middleware
+app.use('/api/v1/blog/*', async (c, next) => {
+  // get the header
+  // verify the header
+  // if header is correct, we can proceed
+  // if not, we return user a 403 status
+
+  const token = c.req.header("Authorization") || "";
+  try {
+    const payload = await verify(token, c.env.JWT_SECRET); // throws an error
+    return c.json({
+      message: "worked"
+    })
+  } catch (e) {
+    c.status(401);
+    return c.json({
+      message: "unauthorized"
+    })
   }
-  await next();
-});
+})
 
 export default app;
